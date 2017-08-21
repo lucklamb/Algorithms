0b50daf08b35fc12cbe05f5c3ae1c37ce5eff7d0
lucklamb <lucklamb@163.com>
2017-08-21 13:25:53 +0800

    edit test.java

---
+++ src/Fundamentals/BinarySearch.java
@@ -0,0 +1,34 @@
+package Fundamentals;
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class BinarySearch {
+
+	public static int rank(int key, int[] a) {
+		int lo = 0;
+		int hi = a.length - 1;
+		while (lo < hi) {
+			int mid = lo + (hi - lo) / 2;
+			if (key < a[mid]) {
+				hi = mid - 1;
+			} else if (key > a[mid]) {
+				lo = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] whitelist = { 23, 54, 12, 63, 14, 25, 73, 13, 79, 5, 21, 9 };
+		Arrays.sort(whitelist);
+		int key = 20;
+		if (rank(key, whitelist) < 0) {
+			StdOut.println(key);
+		}
+	}
+
+}
---
+++ src/Fundamentals/test.java
@@ -0,0 +1,10 @@
+package Fundamentals;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("this is a test");
+
+	}
+
+}
